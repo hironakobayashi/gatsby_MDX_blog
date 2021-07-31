ea7c4c0b2283fe43281ebb587148d7d924397280
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-07-31 15:48:17 +0900

    Post Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,41 @@ import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        body
+      }
+    }
+  }
+`
 
 export default IndexPage
