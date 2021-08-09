aad0f57c7774c949ddeb4f909de6fed05891cd32
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-08-09 13:08:21 +0900

    MDX Wapper

---
+++ root-mdx.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {
+  h3: () => <span>and you thought you will a h3</span>,
+}
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
